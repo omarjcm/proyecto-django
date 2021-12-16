4415454d712919fd6e1fcd8f13b39ed9d2f54b65
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-12-16 18:14:54 -0500

    Configuracion del admin

--- ups/users/admin.py
+++ ups/users/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from users.models import Profile
 
 # Register your models here.
-admin.site.register( Profile )
+
+@admin.register(Profile)
+class ProfileAdmin( admin.ModelAdmin ):
+    """Profile admin."""
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+    search_fields = ('user__enail', 'user__first_name', 'user__last_name', 'phone_number')
+    list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
